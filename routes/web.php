b876cfcfce64fdab23a26c30fbfec00ef14b3849
Sander de Vos <sander@tutanota.de>
2018-11-30 14:07:33 +0100

    Password reset and email verification disabled

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes([
+    'reset'  => false,
+    'verify' => false,
+]);
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
