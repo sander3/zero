1449decab92c32ceb54c15e576c551273a41a87f
Sander de Vos <sander@tutanota.de>
2019-01-17 23:03:25 +0100

    Emoji added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,14 +55,14 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('settings.account.show') }}">{{ __('settings.settings') }}</a>
+                                    <a class="dropdown-item" href="{{ route('settings.account.show') }}">🔧 {{ __('settings.settings') }}</a>
 
                                     <div class="dropdown-divider"></div>
 
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        🚪 {{ __('Logout') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
